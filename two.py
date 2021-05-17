766eb7e4f25fd2b498120748041b0a52bca03d42
Eduard Chuvak <terezamatt@gmail.com>
2021-05-17 13:15:23 +0200

    second try hash sum calculation!

---
+++ two.py
@@ -0,0 +1,37 @@
+import hashlib
+
+checksums = open('checksums', 'r')
+lines = checksums.readlines()
+
+for line in lines:
+  values = line.split(" ")
+  fileName = values[0]
+  hashMechanism = values[1]
+  checksum = values[2]
+
+  hasher = None
+
+  if hashMechanism == 'md5':
+   hasher = hashlib.md5()
+  else:
+   if hashMechanism == 'sha1':
+    hasher = hashlib.sha1()
+   else:
+    print("Wrong hash mechanism selected.")
+
+  buf = None
+
+  try:
+    with open(fileName, 'rb') as afile:
+      buf = afile.read()
+      hasher.update(buf)
+      countedChecksum = hasher.hexdigest()
+      result = None
+      if countedChecksum == checksum:
+       result = "OK"
+      else:
+       result = "FAIL"
+
+      print(fileName + " " + result)
+  except:
+    print(fileName + " NOT EXIST")
